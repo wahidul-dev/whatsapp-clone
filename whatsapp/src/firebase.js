613c034e54fa7b55d1a714e1e84e94b6c526243b
wahidul-dev <wahidul.abir@northsouth.edu>
2020-12-05 19:10:39 +0600

    basic setup

---
+++ whatsapp/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCICvGHO4rPV-K3wDkgbQFvWsvF4JsiIOg",
+    authDomain: "whatsapp-170c4.firebaseapp.com",
+    projectId: "whatsapp-170c4",
+    storageBucket: "whatsapp-170c4.appspot.com",
+    messagingSenderId: "1082039334600",
+    appId: "1:1082039334600:web:2b3764349986fcd064d7fd",
+    measurementId: "G-9QX89B630F"
+  };

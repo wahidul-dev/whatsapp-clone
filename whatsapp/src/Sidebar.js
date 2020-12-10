e1f4c0345b7bf021e980d0a1700099b1cb92ff91
wahidul-dev <wahidul.abir@northsouth.edu>
2020-12-10 18:34:32 +0600

    sidebar added

--- whatsapp/src/Sidebar.js
+++ whatsapp/src/Sidebar.js
@@ -5,6 +5,7 @@ import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { SearchOutlined } from "@material-ui/icons";
 import "./Sidebar.css";
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
     return (
@@ -33,7 +34,11 @@ function Sidebar() {
             </div>
 
             <div className="sidebar__chats">
-
+                <SidebarChat addNewChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                
             </div>
             
         </div>

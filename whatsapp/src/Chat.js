621ca862639a842c386d8590cae7c4ad8178ef62
wahidul-dev <wahidul.abir@northsouth.edu>
2020-12-15 01:54:30 +0600

    chat footer added

--- whatsapp/src/Chat.js
+++ whatsapp/src/Chat.js
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { Avatar, IconButton } from '@material-ui/core';
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 import "./Chat.css";
 
 function Chat() {
-
+    const [input, setInput] = useState("");
     const [seed, setSeed] = useState("");
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
     }, []);
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        console.log("You typed >>> ", input);
+    };
 
     return (
         <div className="chat">
@@ -35,11 +42,25 @@ function Chat() {
             </div>
 
             <div className="chat__body">
-                <p className="chat__message">Hey guys</p>
+                <p className={`chat__message ${true &&
+                "chat__reciever"}`}>
+                <span className="chat__name">Sonny</span>Hey guys
+                <span className="chat__timestamp">
+                    3:52pm
+                </span>
+                </p>
             </div>
 
             <div className="chat__footer">
-
+                <InsertEmoticonIcon />
+                <form>
+                    <input value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        placeholder="Type a message" type="text" />
+                    <button onClick={sendMessage}
+                    type="submit">Send a message</button>
+                </form>
+                <MicIcon />
             </div>
         </div>
     )

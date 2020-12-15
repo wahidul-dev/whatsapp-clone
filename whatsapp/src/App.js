c55991fbe73ea31a478f5b8f0831155f09f91f91
wahidul-dev <wahidul.abir@northsouth.edu>
2020-12-15 22:30:19 +0600

    google authentication added

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,27 +1,36 @@
 import Chat from "./Chat"
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 import Sidebar from "./Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
 
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="app">
-      <div className="app__body">
-      <Router>
-        
-        <Sidebar />
-        <Switch>
-          <Route path="/rooms/:roomId">
-            <Chat />
-          </Route>
-          <Route path="/">
-            <Chat />
-          </Route>
-        </Switch>
-      </Router>
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+        <Router>
+          
+          <Sidebar />
+          <Switch>
+            <Route path="/rooms/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <Chat />
+            </Route>
+          </Switch>
+        </Router>
+        </div>
+      )}
+      
     </div>
   );
 }
